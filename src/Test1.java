e88f8cd9f3f6c253be02117829fe15d40248bf7e
TeamFlex754 <ians.mcvay@gmail.com>
2024-08-10 16:09:08 -0700

    Initial commit

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+        int numDogs = 10;
+        int numCats = 11;
+
+        System.out.println(numDogs + numCats);
+    }
+}
